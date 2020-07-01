255116a941d29fda3fe7057ac994794e1fc84382
AL-Khalil-Developers <alkhalildevelopers@gmail.com>
2020-07-01 17:17:04 +0500

    website added

---
+++ app/src/main/java/com/alkhalildevelopers/quranepak/personal/ChannelPageActivity.java
@@ -0,0 +1,98 @@
+package com.alkhalildevelopers.quranepak.personal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.alkhalildevelopers.apps.quranepak.R;
+import com.alkhalildevelopers.apps.quranepak.ui.QuranActivity;
+
+public class ChannelPageActivity extends AppCompatActivity {
+  SwipeRefreshLayout swipeRefreshLayout;
+  WebView webView;
+  @SuppressLint("SetJavaScriptEnabled")
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_channel_page);
+
+    swipeRefreshLayout = findViewById(R.id.channelSwipeableLayout);
+    webView = findViewById(R.id.channelWebView);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.getSettings().setLoadsImagesAutomatically(true);
+    webView.loadUrl(getResources().getString(R.string.channelUrl));
+
+    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override
+      public void onRefresh() {
+        swipeRefreshLayout.setRefreshing(true);
+        webView.reload();
+      }
+    });
+
+    webView.setWebViewClient(new WebViewClient(){
+      @Override
+      public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        swipeRefreshLayout.setRefreshing(true);
+      }
+
+      @Override
+      public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        swipeRefreshLayout.setRefreshing(false);
+      }
+    });
+
+    webView.setWebChromeClient(new WebChromeClient(){
+      @Override
+      public void onProgressChanged(WebView view, int newProgress) {
+        super.onProgressChanged(view, newProgress);
+        if (newProgress > 50){
+          swipeRefreshLayout.setRefreshing(false);
+        }
+      }
+    });
+
+    webView.setOnKeyListener(new View.OnKeyListener() {
+      @Override
+      public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == MotionEvent.ACTION_UP){
+          if(webView.canGoBack()){
+            webView.goBack();
+          }else {
+            finish();
+          }
+        }
+        int action = event.getAction();
+        AudioManager audioManager =(AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        switch (keyCode) {
+          case KeyEvent.KEYCODE_VOLUME_UP:
+            if (action == KeyEvent.ACTION_DOWN) {
+              audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_RAISE,AudioManager.FLAG_SHOW_UI);
+            }
+            return true;
+          case KeyEvent.KEYCODE_VOLUME_DOWN:
+            if (action == KeyEvent.ACTION_DOWN) {
+              audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_LOWER,AudioManager.FLAG_SHOW_UI);
+            }
+            return true;
+        }
+        return true;
+      }
+    });
+
+
+  }
+}
